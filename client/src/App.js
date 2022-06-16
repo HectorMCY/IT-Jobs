afe2bde4a89ebdaf01b58b2fe03bbe940f2fda8b
Santiago Álvarez <A01640172@itesm.mx>
2022-06-16 09:23:25 -0500

    Intento 3

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,23 @@ function App() {
   const [user, setUser] = useState({id: 0, name: "undefined"});
 
   return(
-    <Home/>
+    <UserContext.Provider value={{user, setUser}}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<home/>}/>
+        <Route path="/registro" element={<Registro/>}/>
+        <Route path="/solicitante" element={<LayoutSolicitante/>}>
+          <Route index element={<DashboardSolicitante/>}/>
+          <Route path="solicitud" element={<CrearSolicitud/>}/>
+          <Route path="perfil" element={<PerfilProfesional/>}/>
+        </Route>
+        <Route path="/reclutador" element={<LayoutReclutador/>}>
+          <Route index element={<DashboardReclutador/>}/>
+          <Route path="vacante" element={<CrearVacante/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </UserContext.Provider>
   )
 }
 
