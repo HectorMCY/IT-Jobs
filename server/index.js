2f270d176014b5625000af88f9383daf95f87393
Santiago Álvarez <A01640172@itesm.mx>
2022-06-16 16:46:20 -0500

    Intento 17

--- server/index.js
+++ server/index.js
@@ -229,7 +229,10 @@ app.put("/solicitud/marcarRevisada/:id", (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 app.listen(PORT, () => {
