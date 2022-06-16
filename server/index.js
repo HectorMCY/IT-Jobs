a7ad15a8bbfc6cfec0b7081bc07ec3216d392131
Santiago Álvarez <A01640172@itesm.mx>
2022-06-16 16:43:24 -0500

    Intento 16

--- server/index.js
+++ server/index.js
@@ -228,7 +228,7 @@ app.put("/solicitud/marcarRevisada/:id", (req, res) => {
 	});
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
